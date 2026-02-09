f0454f7fd92ad8a0bd793f9340cd2cdca22298e5
Sabir <12734@holbertonstudents.com>
2026-02-09 19:24:36 +0400

    I was created a model of the Amenity and endpoints for that.

--- hbnb/app/services/facade.py
+++ hbnb/app/services/facade.py
@@ -1,18 +1,52 @@
-from app.persistence.repository import InMemoryRepository
+from app.persistence.facade import InMemoryRepository
+from app.models.user import User
+from app.models.amenitiy import Amenity
+
 
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-    # Placeholder method for creating a user
     def create_user(self, user_data):
-        # Logic will be implemented in later tasks
-        pass
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
 
-    # Placeholder method for fetching a place by ID
-    def get_place(self, place_id):
-        # Logic will be implemented in later tasks
-        pass
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
+
+    def get_all_users(self):
+        return self.user_repo.get_all()
+
+    def update_user(self, user_id, user_data):
+        user = self.user_repo.get(user_id)
+        if not user:
+            return None
+
+        self.user_repo.update(user_id, user_data)  # Передаем user_data (словарь), а не user
+        return user
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        amenity = self.amenity_repo.get(amenity_id)
+        if not amenity:
+            return None
+
+        self.amenity_repo.update(amenity_id, amenity_data)
+        return amenity
